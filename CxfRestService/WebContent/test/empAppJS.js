bfdec1c0700b109b7870d06735e84d59f886cf94
Mahi <Mahi.Lakkidi@gmail.com>
2016-08-18 14:06:55 -0700

    Adding Karma js files

---
+++ CxfRestService/WebContent/test/empAppJS.js
@@ -0,0 +1,11 @@
+angular.module('empApp',[]).controller('empController',['$scope', '$http', function($scope, $http){
+//var restURL = 'http://localhost:8080/JAXRSWebServiceTestProj-0.0.1-SNAPSHOT/rest/factorialService/factorial?inputValue=';	
+var restURL = 'http://localhost:8080/CXFRestServiceProj/rest/employeeService/getemployeedetail?employeeId='
+$scope.isEmp = false;
+$scope.getEmp = function(){
+$http.get(restURL.concat($scope.empID)).then(function(response){
+$scope.employees = response.data;
+$scope.isEmp = false;
+},function(error){$scope.isEmp = true;})};
+}]);
+
---
+++ CxfRestService/WebContent/test/empAppJasmine.js
@@ -0,0 +1,27 @@
+describe('Employee Rest Example', function () {
+
+  beforeEach(module('empApp'));
+
+  var $controller;
+  var mockEmpList = [{"firstName":"Pete","lastName":"Smith","employeeId":"123456","email":"test@example.com","dateOfJoining":"01-02-2001","department":"Sales"}];
+  beforeEach(inject(function(_$controller_, _$httpBackend_){
+    $controller = _$controller_;
+	$httpBackend = _$httpBackend_;
+	$httpBackend.whenGET('http://localhost:8080/CXFRestServiceProj/rest/employeeService/getemployeedetail?employeeId=123456').respond(200, mockEmpList);
+  }));
+
+  describe('empApp module test', function () {
+  it('says hello employees!', function () {
+      var $scope = {};
+      var controller = $controller('empController', { $scope: $scope });
+	  $scope.empID = "123456";
+	  $scope.getEmp();
+	  $httpBackend.flush();
+	  expect($scope.employees).not.toEqual(undefined);
+    }); 
+  });
+
+});
+
+
+ 
