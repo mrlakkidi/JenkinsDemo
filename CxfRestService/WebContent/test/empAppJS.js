c1e6d8617af250a63891ee63c18d54fab0ea6701
Mahi <Mahi.Lakkidi@gmail.com>
2016-08-18 23:04:37 -0700

    JS Test pass

--- CxfRestService/WebContent/test/empAppJS.js
+++ CxfRestService/WebContent/test/empAppJS.js
@@ -1,6 +1,6 @@
 angular.module('empApp',[]).controller('empController',['$scope', '$http', function($scope, $http){
 //var restURL = 'http://localhost:8080/JAXRSWebServiceTestProj-0.0.1-SNAPSHOT/rest/factorialService/factorial?inputValue=';	
-var restURL = 'http://localhost:8080/CXFRestServiceProj/rest/employeeService/getemployeedetail?employeeI='
+var restURL = 'http://localhost:8080/CXFRestServiceProj/rest/employeeService/getemployeedetail?employeeId='
 $scope.isEmp = false;
 $scope.getEmp = function(){
 $http.get(restURL.concat($scope.empID)).then(function(response){
